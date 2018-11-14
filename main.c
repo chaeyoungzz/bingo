7fa0f02435ffe37b17121efbc8b478e01ea1d1bd
chaeyoung <456codud@naver.com>
2018-11-14 17:43:46 +0900

    bingo
    get number by me 코드 수정중,,,,

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 2
+#define N 4
+#define M 3
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 initiate_bingo(int B[][N], int CB[][N]) 
@@ -19,7 +20,7 @@ initiate_bingo(int B[][N], int CB[][N])
 	}
 }
 
-print_bingo(int BINGO[][N])
+print_bingo(int BINGO[][N]) 
 {
 	int i, j;
 	
@@ -27,29 +28,52 @@ print_bingo(int BINGO[][N])
 	{
 		for( j=0 ; j<N ; j++)
 		{
-			printf("%i ", BINGO[i][j]);
+			printf(" %i ", BINGO[i][j]);
 		}
 		printf("\n");
 	}
 	printf("\n");
  } 
  
-get_number_byMe(int k, int B[][N])
+get_number_byMe( int B[][N])
 {
  	int i, j ;
- 		 
-	if(k==-1)
+ 	int NUM;
+ 	
+ 	printf("input number = ");
+	scanf("%i", &NUM);
+	
+ 	if ( NUM < 1 || NUM > N*N)
 	{
-	 	printf("이미 선택한 숫자 입니다 ");
+		printf(" 범위 외의 숫자 입니다 \n");
 	}
-	 
-	else if (k>N*N+1)
-	{
-		printf(" 선택 범위를 넘어선 숫자 입니다 ");
-	}
+ 	
+    else
+    {
+    	for( i=0 ; i<N ; i++)
+	    {
+		    for( j=0 ; j<N ; j++)
+		    {
+			    if( NUM==B[i][j] )
+			    {
+			    	printf(" 나의 빙고 \n");
+			    	process_bingo( B, NUM);
+		            print_bingo( B );
+		            break;
+				}
+				else
+				{
+					printf("  1 이미 선택한 숫자 입니다.\n");
+					break; 
+				}
+		    }
+	    }  
+	}    
+	
+ 
 }
 
-get_number_byCom(int B[][N])
+get_number_byCom(int CB[][N])
 {
 	int i, j ;
 	int k;
@@ -57,18 +81,35 @@ get_number_byCom(int B[][N])
 	srand(time(NULL));
 	k = rand() %N*N + 1 ;
 	 
-	if(k==-1)
+	if ( k < 1 || k > N*N)
 	{
-	 	printf("이미 선택한 숫자 입니다 ");
+		printf(" 범위 외의 숫자 입니다 \n");
 	}
-	 
-	else if (k>N*N+1)
-	{
-		printf(" 선택 범위를 넘어선 숫자 입니다 ");
-	}
+ 	
+    else
+    {
+    	for( i=0 ; i<N ; i++)
+	    {
+		    for( j=0 ; j<N ; j++)
+		    {
+			    if( k==CB[i][j] )
+			    {
+			    	printf(" C 빙고 \n");
+					process_bingo( CB, k);
+		            print_bingo( CB );
+				}
+				else
+				{
+					printf(" 이미 선택한 숫자 입니다.\n");
+					break; 
+				}
+		    }
+	    }  
+	}    
+	
 } 
 
-process_bingo(int B[][N], int k)
+process_bingo(int BINGO[][N], int num)
 {
 	int i, j ;
 	
@@ -76,37 +117,49 @@ process_bingo(int B[][N], int k)
  		{
  			for( j=0 ; j<N ; j++)
 			{
-			 	if ( B[i][j]==k )
+			 	if ( BINGO[i][j]== num )
 			 	{
-			 		B[i][j]=-1;
+			 		BINGO[i][j]=-1;
 				}
 			}
 		}
 } 
 
+count_bingo(int k)
+{
+	
+}
+
 int main(int argc, char *argv[]) 
 {
 	int bingo_game[N][N];
 	int C_bingo_game[N][N];
-	int num;
 	
 	initiate_bingo( bingo_game, C_bingo_game ) ;
 	
 	print_bingo(bingo_game);
 	print_bingo(C_bingo_game);
 	
-	printf("input number = ");
-	scanf("%i", &num);
-	
-	get_number_byMe(num, bingo_game);
+		
+	get_number_byMe(bingo_game);
+	get_number_byCom(C_bingo_game);
+
+
+
+	get_number_byMe(bingo_game);
 	get_number_byCom(C_bingo_game);
 	
-	process_bingo(bingo_game,num);
-	process_bingo(C_bingo_game, num);
-	
-	print_bingo(bingo_game);
-	print_bingo(C_bingo_game);
 	
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+

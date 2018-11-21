6047252b78c192e177de578bbccd4932010d1012
chaeyoung <456codud@naver.com>
2018-11-21 19:39:31 +0900

    print_bingo

--- main 2.c
+++ main 2.c
@@ -6,35 +6,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-initiate_bingo(int BINGO[][N])  
+print_bingo(int BINGO[][N], int NAME) 
 {
-	int x, y=1, i, j;
- 	int k=0, w=N*N-1, T;
- 	int G[N*N]; // 1~N*N 숫자 불러오기 위한 배열  
- 	int p;
- 	
- 	// 1~N*N 숫자 불러옴  
- 	for (x=0 ; x<N*N ; x++)
- 	{
- 		G[x]=y;
- 		y++;
-	}
-	 
-	for( i=0 ; i<N ; i++)
+	int i, j;
+	
+	if (NAME == 1) // 알아보기 쉽게 이름을 정해주려고,,,, 
 	{
-	    for( j=0 ; j<N ; j++)
-		{
-			BINGO[i][j] = G[T=rand()%(N*N-k)];	//불러온 숫자를 BINGO 배열에 각각 할당  
-			
-			// 선택된 배열을 맨 뒤의 배열과 값을 바꿔줌
-			// ex) G[2]가 선택되었다고 하면 G[N*N-1] 의 값과 바꿔줌 그 다음은 G[N*N-2] 와 값을 바꿈  
-			p = G[w];
-	        G[w] = G[T];
-	        G[T] = p;
-	        k++;  // rand 의 범위를 바꿔줌 이미 선택된 숫자 선택 하지 못하게 해줌  
-	        w--;  // 배열 맨 뒤 부터 순차적으로 할당하기 위해  G[N*N-1] , G[N*N-2], G[N*N-3] ----   
-		}
+		printf("<YOU>\n");
+	}
+	else
+	{
+		printf("<COM>\n");
 	}
 	
-}
-
+	// 실제 배열을 프린트 하는 코드  
+	for( i=0 ; i<N ; i++)
+	{
+		for( j=0 ; j<N ; j++)
+		{
+			printf(" %3d ", BINGO[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+ } 
